db522b709c7d10a181fb93d1826e14fa47aa311b
Umair <umairrafiq156@gmail.com>
2025-02-23 13:29:48 +0530

    removed cache files

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c1c3c3", "#091112" },
-	[SchemeSel] = { "#c1c3c3", "#457857" },
-	[SchemeOut] = { "#c1c3c3", "#A7CB89" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#091112";
-static const char* selfgcolor   = "#c1c3c3";
-static const char* normbgcolor  = "#54815b";
-static const char* normfgcolor  = "#c1c3c3";
-static const char* urgbgcolor   = "#457857";
-static const char* urgfgcolor   = "#c1c3c3";
