9a8311f8ac29b6fcddcd387807ff6569e83098f2
Umair <umairrafiq156@gmail.com>
2025-10-13 13:35:36 +0530

    Keep it clean

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -26,44 +26,17 @@ return {
             },
         })
 
-        -- Add cmp_nvim_lsp capabilities settings to lspconfig
-        -- This should be executed before you configure any language server
-        local lspconfig_defaults = require('lspconfig').util.default_config
-        lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-            'force',
-            lspconfig_defaults.capabilities,
-            require('cmp_nvim_lsp').default_capabilities()
-        )
-
-        -- This is where you enable features that only work
-        -- if there is a language server active in the file
-        vim.api.nvim_create_autocmd('LspAttach', {
-            callback = function(event)
-                local opts = { buffer = event.buf }
-
-                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-            end,
-        })
-
-        require('lspconfig').clangd.setup({})
-        require('lspconfig').pyright.setup({})
-        require('lspconfig').lua_ls.setup({})
-        require('lspconfig').bashls.setup({})
-        require('lspconfig').html.setup({})
-        require('lspconfig').hls.setup({})
-        require('lspconfig').ts_ls.setup({})
-        require('lspconfig').cssls.setup({})
-        require('lspconfig').emmet_language_server.setup({
+        -- Configure Lsp
+        vim.lsp.enable('clangd')
+        vim.lsp.enable('pyright')
+        vim.lsp.enable('lua_ls')
+        vim.lsp.enable('bashls')
+        vim.lsp.enable('html')
+        vim.lsp.enable('hls')
+        vim.lsp.enable('ts_ls')
+        vim.lsp.enable('cssls')
+        vim.lsp.enable('jdtls')
+        vim.lsp.config['emmet_language_server'] = {
             filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
             init_options = {
                 ---@type table<string, string>
@@ -85,7 +58,8 @@ return {
                 --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
                 variables = {},
             },
-        })
+        }
+        vim.lsp.enable('emmet_language_server')
 
         local cmp = require('cmp')
 
